60e8b7ffbf707fc75011a658f9091f763269c207
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-30 17:09:41 +0200

    npm install rxjs-compat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+
+interface Dvd {
+  title: string;
+  content: string;
+}
+interface DvdId extends Dvd {
+  id: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DVD-firestore';
+
+  titleApp = 'Dvd app Dominik Palacz';
+
+  dvdCollection: AngularFirestoreCollection<Dvd>;
+  dvd: any; //Observable<Dvd[]>;
+
+  id: string;
+  title: string;
+
+  constructor(private afs: AngularFirestore){
+
+  }
+
+  ngOnInit(){
+    this.dvdCollection = this.afs.collection('dvd');
+    //this.dvd = this.dvdCollection.valueChanges();
+    this.dvd = this.dvdCollection.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Dvd;
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+      })
+  }
+
+  addDvd(){
+    this.afs.collection('dvd').add({'id': this.id, 'title': this.title})
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAmON0ZMCgdy2Nqs-DgfC5_T0d_tGj7Gbs",
+  authDomain: "dvd-firestore.firebaseapp.com",
+  databaseURL: "https://dvd-firestore.firebaseio.com",
+  projectId: "dvd-firestore",
+  storageBucket: "dvd-firestore.appspot.com",
+  messagingSenderId: "298057613832"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
