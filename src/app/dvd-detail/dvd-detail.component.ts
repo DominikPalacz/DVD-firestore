2638b16601dae292553c9054dfaf043d00420a6a
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-07-31 09:14:41 +0200

    styled

--- src/app/dvd-detail/dvd-detail.component.ts
+++ src/app/dvd-detail/dvd-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dvd } from '../app.component';
 
 @Component({
   selector: 'app-dvd-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dvd-detail.component.css']
 })
 export class DvdDetailComponent implements OnInit {
+
+  @Input() dvd: Dvd;
 
   constructor() { }
 
